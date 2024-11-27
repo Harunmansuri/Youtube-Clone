964bec0e87fe3e54bd8b792401a7a50176f095da
Harunmansuri <141892054+Harunmansuri@users.noreply.github.com>
2024-11-27 16:00:06 +0530

    navbar component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import NavBar from './Components/Navbar/NavBar.jsx'
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+    <NavBar/>
+    </>
   )
 }
 
