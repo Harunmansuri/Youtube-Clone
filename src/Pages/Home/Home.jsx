64c5f491ae3768bd70b374f2ffc1ec7bd37d9060
Harunmansuri <141892054+Harunmansuri@users.noreply.github.com>
2024-11-28 09:58:03 +0530

    Feed section done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,15 @@
+import Feed from '../../Components/Feed/Feed'
 import SideBar from '../../Components/Sidebar/SideBar'
 import './Home.css'
 import React from 'react'
 
-const Home = () => {
+const Home = (sidebar) => {
   return (
     <>
-      <SideBar/>
+      <SideBar sidebar={sidebar}/>
+      <div className={`container ${sidebar? "":'large-container'}`}>
+        <Feed/>
+      </div>
     </>
   )
 }
